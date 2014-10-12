40aa210dbbb9929511e0326d959476b28d863168
JesseLivezey <jesse.livezey@gmail.com>
2014-10-12 10:55:46 -0700

    updated script

--- pylearn2/scripts/balance/constrained_autoencoder.py
+++ pylearn2/scripts/balance/constrained_autoencoder.py
@@ -1,17 +1,18 @@
 from pylearn2.config import yaml_parse
 import numpy as np
-with open('semi2.yaml', 'rb') as f:
+with open('semi.yaml', 'rb') as f:
     train = f.read()
     
 init_alpha = .1
 dim = 2304
 
-L0dim = 2000
-L1dim = 2000
-zdim = 793
+init_bias = 1.
+L0dim = 400
+L1dim = 400
+zdim = 393
 ydim=7
-L_2dim=2000
-L_1dim=2000
+L_2dim=1000
+L_1dim=1000
 max_col_norm = .863105108422
 
 params = {'L0dim': L0dim,
@@ -26,7 +27,8 @@ params = {'L0dim': L0dim,
           'L_2std': np.sqrt(init_alpha/(ydim+zdim+L_2dim)),
           'L_1std': np.sqrt(init_alpha/(L_1dim+L_2dim)),
           'L_0std': np.sqrt(init_alpha/(dim+L_1dim)),
-          'max_col_norm': max_col_norm
+          'max_col_norm': max_col_norm,
+          'init_bias': init_bias
           }
 train = train % params
 print train
