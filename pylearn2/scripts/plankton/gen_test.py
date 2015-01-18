0c6fe37e07b7297726bd1d97e1006c004fde7fd3
briancheung <bcheung5@gmail.com>
2015-01-18 12:56:48 -0800

    normalized imageset generation

--- pylearn2/scripts/plankton/gen_test.py
+++ pylearn2/scripts/plankton/gen_test.py
@@ -7,7 +7,7 @@ if len(sys.argv) < 3:
 fi = sys.argv[1]
 fo = sys.argv[2]
 
-cmd = "convert -resize 48x48 -gravity center -background white -extent 48x48 "
+cmd = "convert -resize 48x48\! "
 imgs = os.listdir(fi)
 
 for img in imgs:
