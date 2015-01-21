173afcb8a5d16e056a24a15b866bbbb8ff7d95fd
JesseLivezey <jesse.livezey@gmail.com>
2015-01-20 17:10:34 -0800

    augmentation

--- pylearn2/data_augmentation.py
+++ pylearn2/data_augmentation.py
@@ -2,7 +2,7 @@ from pylearn2.blocks import Block
 import numpy as np
 from pylearn2.utils.rng import make_np_rng
 from pylearn2.space import Conv2DSpace
-from scipy.ndimage.interpolation import rotate, shift
+from scipy.ndimage.interpolation import rotate, shift, zoom
 
 class DataAugmentation(Block):
 
@@ -20,6 +20,8 @@ class DataAugmentation(Block):
         self.deg = 360*self.rng.rand()
         self.axis0 = self.rng.uniform(low=-3., high=3.)
         self.axis1 = self.rng.uniform(low=-3., high=3.)
+        self.zoom0 = self.rng.uniform(low=.8, high=1.2)
+        self.zoom1 = self.rng.uniform(low=.8, high=1.2)
 
     def perform(self, X):
         X = np.transpose(X, axes=(1,2,3,0))
@@ -29,13 +31,16 @@ class DataAugmentation(Block):
                    angle=self.deg,
                    order=self.spline_order,
                    cval=self.cval,
-                   reshape=False
-                   )
+                   reshape=False)
+        X = zoom(X,
+                 zoom(self.zoom0,
+                      self.zoom1,
+                      order=self.spline_order,
+                      cval=self.cval)
         X = shift(X,
                   shift=(self.axis0, self.axis1, 0,0),
                   order=self.spline_order,
-                  cval=self.cval
-                  )
+                  cval=self.cval)
         X = np.transpose(X, axes=(3,0,1,2))
         return X
 
